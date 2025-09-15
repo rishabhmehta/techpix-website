6939ac464d54bcea5ae6abc5c7db5940afa2b5ce
Ritik Sachan <sachanritik1@gmail.com>
2025-09-15 17:30:21 +0530

    feat: update theme toggle button icons with transition effects

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -20,8 +20,11 @@ export function ThemeToggle() {
       className="relative cursor-pointer"
       disabled={!mounted}
     >
-      <Sun className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute size-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      {current === 'dark' ? (
+        <Sun className="size-4" />
+      ) : (
+        <Moon className="size-4" />
+      )}
     </Button>
   );
 }
